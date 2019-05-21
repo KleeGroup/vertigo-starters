1560a26fa85326eca4501f8c08dd7f01ee51e728
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-05-21 11:49:23 +0200

    vertigo 2.0 archetypes

---
+++ vertigo-project-archetypes/vertigo-ui-archetype/src/main/resources/archetype-resources/src/main/java/__projectNameLowerCase__/__businessModuleLowerCase__/services/MovieServicesImpl.java
@@ -0,0 +1,32 @@
+package ${package}.${projectNameLowerCase}.${businessModuleLowerCase}.services;
+
+import javax.inject.Inject;
+
+import io.vertigo.dynamo.domain.model.DtList;
+import io.vertigo.dynamo.domain.model.DtListState;
+import io.vertigo.dynamo.criteria.Criterions;
+import io.vertigo.commons.transaction.Transactional;
+import ${package}.${projectNameLowerCase}.${businessModuleLowerCase}.dao.MovieDAO;
+import ${package}.${projectNameLowerCase}.${businessModuleLowerCase}.domain.Movie;
+
+@Transactional
+public class MovieServicesImpl implements MovieServices {
+
+	@Inject
+	private MovieDAO movieDAO;
+
+	@Override
+	public Movie get(final Long movId) {
+		return movieDAO.get(movId);
+	}
+
+	@Override
+	public void save(final Movie movie) {
+		movieDAO.save(movie);
+	}
+
+	@Override
+	public DtList<Movie> getMovies(final DtListState dtListState) {
+		return movieDAO.findAll(Criterions.alwaysTrue(), dtListState);
+	}
+}
---
+++ vertigo-project-archetypes/vertigo-vega-archetype/src/main/resources/archetype-resources/src/main/java/__projectNameLowerCase__/mda/Studio.java
@@ -0,0 +1,74 @@
+package ${package}.${projectNameLowerCase}.mda;
+
+import javax.inject.Inject;
+
+import io.vertigo.app.AutoCloseableApp;
+import io.vertigo.app.config.DefinitionProviderConfig;
+import io.vertigo.app.config.ModuleConfig;
+import io.vertigo.app.config.NodeConfig;
+import io.vertigo.commons.CommonsFeatures;
+import io.vertigo.core.param.Param;
+import io.vertigo.core.plugins.resource.classpath.ClassPathResourceResolverPlugin;
+import io.vertigo.dynamo.DynamoFeatures;
+import io.vertigo.dynamo.plugins.environment.DynamoDefinitionProvider;
+import io.vertigo.studio.StudioFeatures;
+import io.vertigo.studio.mda.MdaManager;
+import io.vertigo.util.InjectorUtil;
+
+public class Studio {
+
+	private static NodeConfig buildNodeConfig() {
+		return NodeConfig.builder()
+				.beginBoot()
+				.withLocales("fr_FR")
+				.addPlugin(ClassPathResourceResolverPlugin.class)
+				.endBoot()
+				.addModule(new CommonsFeatures()
+						.withCache()
+						.withMemoryCache()
+						.withScript()
+						.withJaninoScript()
+						.build())
+				.addModule(new DynamoFeatures().build())
+				//----Definitions
+				.addModule(ModuleConfig.builder("ressources")
+						.addDefinitionProvider(DefinitionProviderConfig.builder(DynamoDefinitionProvider.class)
+								.addDefinitionResource("kpr", "${packageInPathFormat}/${projectNameLowerCase}/gen.kpr")
+								.build())
+						.build())
+				// ---StudioFeature
+				.addModule(new StudioFeatures()
+						.withMasterData()
+						.withMda(
+								Param.of("projectPackageName", "${package}.${projectNameLowerCase}"))
+						.withJavaDomainGenerator(
+								Param.of("generateDtResources", "false"))
+						.withTaskGenerator()
+						.withSearchGenerator()
+						.withFileGenerator()
+						.withSqlDomainGenerator(
+								Param.of("baseCible", "Postgres"),
+								Param.of("generateDrop", "false"),
+								Param.of("generateMasterData", "true"))
+						.build())
+				.build();
+
+	}
+
+	@Inject
+	private MdaManager mdaManager;
+
+	public static void main(final String[] args) {
+		try (final AutoCloseableApp app = new AutoCloseableApp(buildNodeConfig())) {
+			final Studio sample = new Studio();
+			InjectorUtil.injectMembers(sample);
+			//-----
+			sample.cleanGenerate();
+		}
+	}
+
+	void cleanGenerate() {
+		mdaManager.clean();
+		mdaManager.generate().displayResultMessage(System.out);
+	}
+}
